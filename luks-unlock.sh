a98ea62c43d86d313503abbe304d6e81f007fb3e
Philipp Schmitt <philipp@schmitt.co>
2020-10-29 19:42:44 +0100

    Don't start service when sourcing file

--- luks-unlock.sh
+++ luks-unlock.sh
@@ -60,148 +60,132 @@ log-notify() {
   fi
 }
 
-if [[ -n "$*" ]]
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
+  if [[ -n "$*" ]]
+  then
+    while true
+    do
+      case "$1" in
+        --help|-h)
+          usage
+          exit 0
+          ;;
+        --host|-H)
+          SSH_HOST="$2"
+          shift 2
+          ;;
+        --port|-P)
+          SSH_PORT="$2"
+          shift 2
+          ;;
+        --username|--user|-u)
+          SSH_USER="$2"
+          shift 2
+          ;;
+        --ssh-key|--key|--private-key|--pkey|-k)
+          SSH_KEY="$2"
+          shift 2
+          ;;
+        --sleep-interval|--sleep|-s)
+          SLEEP_INTERVAL="$2"
+          shift 2
+          ;;
+        --luks-type|--type|-t)
+          LUKS_TYPE="$2"
+          shift 2
+          ;;
+        --luks-password|--password|-p)
+          LUKS_PASSWORD="$2"
+          shift 2
+          ;;
+        --event-file|--event|-e)
+          EVENTS_FILE="$2"
+          shift 2
+          ;;
+        --apprise-url|--apprise|-a)
+          APPRISE_URL="$2"
+          shift 2
+          ;;
+        --apprise-tag|--tag)
+          APPRISE_TAG="$2"
+          shift 2
+          ;;
+        --apprise-title|--title)
+          APPRISE_TITLE="$2"
+          shift 2
+          ;;
+        *)
+          usage >&2
+          exit 2
+          break
+          ;;
+      esac
+    done
+  fi
+
+  if [[ -n "$DEBUG" ]]
+  then
+    {
+      echo "DEBUG: Secrets"
+      ls -l /run/secrets
+    } >&2
+  fi
+
+  if [[ ! -r "$SSH_KEY" ]]
+  then
+    echo "SSH_KEY at $SSH_KEY is not readable." >&2
+    exit 2
+  fi
+
+  # Copy file locally and correct mode
+  if [[ "$(stat -c "%a" "$SSH_KEY")" != "400" ]]
+  then
+    cp "$SSH_KEY" /tmp
+    SSH_KEY="/tmp/$(basename "$SSH_KEY")"
+    chmod 400 "$SSH_KEY"
+  fi
+
+  if [[ -z "$LUKS_PASSWORD" ]] && [[ -n "$LUKS_PASSWORD_FILE" ]]
+  then
+    if [[ ! -r "$LUKS_PASSWORD_FILE" ]]
+    then
+      echo "$LUKS_PASSWORD_FILE: No such file or directory" >&2
+      exit 3
+    fi
+    LUKS_PASSWORD="$(cat "$LUKS_PASSWORD_FILE")"
+  fi
+
+  if [[ -z "$LUKS_PASSWORD" ]]
+  then
+    echo "LUKS_PASSWORD is not set." >&2
+    exit 2
+  fi
+
+  log "LUKS rigmarole started. I'll be trying to unlock ${SSH_HOST}"
+
   while true
   do
-    case "$1" in
-      --help|-h)
-        usage
-        exit 0
-        ;;
-      --host|-H)
-        SSH_HOST="$2"
-        shift 2
-        ;;
-      --port|-P)
-        SSH_PORT="$2"
-        shift 2
-        ;;
-      --username|--user|-u)
-        SSH_USER="$2"
-        shift 2
-        ;;
-      --ssh-key|--key|--private-key|--pkey|-k)
-        SSH_KEY="$2"
-        shift 2
-        ;;
-      --sleep-interval|--sleep|-s)
-        SLEEP_INTERVAL="$2"
-        shift 2
-        ;;
-      --luks-type|--type|-t)
-        LUKS_TYPE="$2"
-        shift 2
-        ;;
-      --luks-password|--password|-p)
-        LUKS_PASSWORD="$2"
-        shift 2
-        ;;
-      --event-file|--event|-e)
-        EVENTS_FILE="$2"
-        shift 2
-        ;;
-      --apprise-url|--apprise|-a)
-        APPRISE_URL="$2"
-        shift 2
-        ;;
-      --apprise-tag|--tag)
-        APPRISE_TAG="$2"
-        shift 2
-        ;;
-      --apprise-title|--title)
-        APPRISE_TITLE="$2"
-        shift 2
-        ;;
-      *)
-        usage >&2
-        exit 2
-        break
-        ;;
-    esac
-  done
-fi
-
-if [[ -n "$DEBUG" ]]
-then
-  {
-    echo "DEBUG: Secrets"
-    ls -l /run/secrets
-  } >&2
-fi
-
-if [[ ! -r "$SSH_KEY" ]]
-then
-  echo "SSH_KEY at $SSH_KEY is not readable." >&2
-  exit 2
-fi
-
-# Copy file locally and correct mode
-if [[ "$(stat -c "%a" "$SSH_KEY")" != "400" ]]
-then
-  cp "$SSH_KEY" /tmp
-  SSH_KEY="/tmp/$(basename "$SSH_KEY")"
-  chmod 400 "$SSH_KEY"
-fi
-
-if [[ -z "$LUKS_PASSWORD" ]] && [[ -n "$LUKS_PASSWORD_FILE" ]]
-then
-  if [[ ! -r "$LUKS_PASSWORD_FILE" ]]
-  then
-    echo "$LUKS_PASSWORD_FILE: No such file or directory" >&2
-    exit 3
-  fi
-  LUKS_PASSWORD="$(cat "$LUKS_PASSWORD_FILE")"
-fi
-
-if [[ -z "$LUKS_PASSWORD" ]]
-then
-  echo "LUKS_PASSWORD is not set." >&2
-  exit 2
-fi
-
-log "LUKS rigmarole started. I'll be trying to unlock ${SSH_HOST}"
-
-while true
-do
-  # Perform Healthcheck if required
-  if [[ -n "$HEALTHCHECK_PORT" ]]
-  then
-    if nc -z -w 2 "$SSH_HOST" "$HEALTHCHECK_PORT"
+    # Perform Healthcheck if required
+    if [[ -n "$HEALTHCHECK_PORT" ]]
     then
-      if [[ -n "$DEBUG" ]]
+      if nc -z -w 2 "$SSH_HOST" "$HEALTHCHECK_PORT"
       then
-        log "Healthcheck result OK"
-      fi
-      sleep "$SLEEP_INTERVAL"
-      continue
-    fi
-  fi
-
-  if nc -z -w 2 "$SSH_HOST" "$SSH_PORT"
-  then
-    log "Trying to unlock remotely ${SSH_HOST}"
-
-    case "$LUKS_TYPE" in
-      direct)
-        if echo "$LUKS_PASSWORD" | \
-             ssh -F /dev/null \
-               -o ConnectTimeout=5 \
-               -o UserKnownHostsFile=/dev/null \
-               -o StrictHostKeyChecking=no \
-               -t \
-               -i "$SSH_KEY" \
-               -l "$SSH_USER" \
-               "$SSH_HOST"
+        if [[ -n "$DEBUG" ]]
         then
-          log-notify "LUKS unlocked host at $SSH_HOST"
-        else
-          log-notify "Failed to unlock $SSH_HOST" >&2
+          log "Healthcheck result OK"
         fi
-        ;;
-      # TODO test
-      # https://github.com/gsauthof/dracut-sshd/issues/32
-      dracut-systemd|dracut-sshd|dracut|alt)
+        sleep "$SLEEP_INTERVAL"
+        continue
+      fi
+    fi
+
+    if nc -z -w 2 "$SSH_HOST" "$SSH_PORT"
+    then
+      log "Trying to unlock remotely ${SSH_HOST}"
+
+      case "$LUKS_TYPE" in
+        direct)
           if echo "$LUKS_PASSWORD" | \
                ssh -F /dev/null \
                  -o ConnectTimeout=5 \
@@ -210,17 +194,36 @@ do
                  -t \
                  -i "$SSH_KEY" \
                  -l "$SSH_USER" \
-                 "$SSH_HOST" systemd-tty-ask-password-agent
-        then
-          log-notify "LUKS unlocked host at $SSH_HOST"
-        else
-          log-notify "Failed to unlock $SSH_HOST" >&2
-        fi
-        ;;
-    esac
-  else
-    log "$SSH_HOST is not reachable on port $SSH_PORT"
-  fi
+                 "$SSH_HOST"
+          then
+            log-notify "LUKS unlocked host at $SSH_HOST"
+          else
+            log-notify "Failed to unlock $SSH_HOST" >&2
+          fi
+          ;;
+        # TODO test
+        # https://github.com/gsauthof/dracut-sshd/issues/32
+        dracut-systemd|dracut-sshd|dracut|alt)
+            if echo "$LUKS_PASSWORD" | \
+                 ssh -F /dev/null \
+                   -o ConnectTimeout=5 \
+                   -o UserKnownHostsFile=/dev/null \
+                   -o StrictHostKeyChecking=no \
+                   -t \
+                   -i "$SSH_KEY" \
+                   -l "$SSH_USER" \
+                   "$SSH_HOST" systemd-tty-ask-password-agent
+          then
+            log-notify "LUKS unlocked host at $SSH_HOST"
+          else
+            log-notify "Failed to unlock $SSH_HOST" >&2
+          fi
+          ;;
+      esac
+    else
+      log "$SSH_HOST is not reachable on port $SSH_PORT"
+    fi
 
-  sleep "$SLEEP_INTERVAL"
-done
+    sleep "$SLEEP_INTERVAL"
+  done
+fi
