f659837c4480c75f9f832cfb97d8f3eb93c00501
Philipp Schmitt <philipp@schmitt.co>
2022-10-31 14:22:55 +0100

    The LUKS and healthchecks hosts can be different

--- luks-unlock.sh
+++ luks-unlock.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-SSH_HOST="${SSH_HOST:-example.com}"
+SSH_HOSTNAME="${SSH_HOSTNAME:-example.com}"
 SSH_KEY="${SSH_KEY:-/run/secrets/ssh_key}"
 SSH_PORT="${SSH_PORT:-22}"
-SSH_USER="${SSH_USER:-root}"
+SSH_USERNAME="${SSH_USERNAME:-root}"
 
 LUKS_PASSWORD="${LUKS_PASSWORD}"
-LUKS_PASSWORD_FILE="${LUKS_PASSWORD_FILE=-/run/secrets/luks_password_${SSH_HOST}}"
+LUKS_PASSWORD_FILE="${LUKS_PASSWORD_FILE=-/run/secrets/luks_password_${SSH_HOSTNAME}}"
 LUKS_TYPE="${LUKS_TYPE:-direct}"
 
 EVENTS_FILE="${EVENTS_FILE}"
-SLEEP_INTERVAL="${SLEEP_INTERVAL:-5}"
+SLEEP_INTERVAL="${SLEEP_INTERVAL:-10}"
 
 HEALTHCHECK_PORT="${HEALTHCHECK_PORT}"
 HEALTHCHECK_REMOTE_CMD="${HEALTHCHECK_REMOTE_CMD}"
@@ -90,8 +90,8 @@ _ssh() {
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
     -i "$SSH_KEY" \
-    -l "$SSH_USER" \
-    "$SSH_HOST" \
+    -l "$SSH_USERNAME" \
+    "$SSH_HOSTNAME" \
     "$@"
 }
 
@@ -132,76 +132,95 @@ luks_unlock() {
 
       _ssh systemctl restart "systemd-cryptsetup@${mapper}"
       ;;
+
     # https://github.com/gsauthof/dracut-sshd/issues/32
     dracut-systemd|dracut-sshd|dracut|alt)
       _ssh systemd-tty-ask-password-agent <<< "$LUKS_PASSWORD"
+      ;;
+
+    # https://github.com/pschmitt/luks-mount.sh
+    luks-mount)
+      _ssh luks-mount <<< "$LUKS_PASSWORD"
       ;;
   esac
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-  if [[ -n "$*" ]]
-  then
-    while true
-    do
-      case "$1" in
-        --help|-h)
-          usage
-          exit 0
-          ;;
-        --host|-H)
-          SSH_HOST="$2"
-          shift 2
-          ;;
-        --port|-P)
-          SSH_PORT="$2"
-          shift 2
-          ;;
-        --username|--user|-u)
-          SSH_USER="$2"
-          shift 2
-          ;;
-        --ssh-key|--key|--private-key|--pkey|-k)
-          SSH_KEY="$2"
-          shift 2
-          ;;
-        --sleep-interval|--sleep|-s)
-          SLEEP_INTERVAL="$2"
-          shift 2
-          ;;
-        --luks-type|--type|-t)
-          LUKS_TYPE="$2"
-          shift 2
-          ;;
-        --luks-password|--password|-p)
-          LUKS_PASSWORD="$2"
-          shift 2
-          ;;
-        --event-file|--event|-e)
-          EVENTS_FILE="$2"
-          shift 2
-          ;;
-        --apprise-url|--apprise|-a)
-          APPRISE_URL="$2"
-          shift 2
-          ;;
-        --apprise-tag|--tag)
-          APPRISE_TAG="$2"
-          shift 2
-          ;;
-        --apprise-title|--title)
-          APPRISE_TITLE="$2"
-          shift 2
-          ;;
-        *)
-          usage >&2
-          exit 2
-          break
-          ;;
-      esac
-    done
-  fi
+  while [[ -n "$*" ]]
+  do
+    case "$1" in
+      --help|-h)
+        usage
+        exit 0
+        ;;
+      --debug|-D)
+        DEBUG=1
+        shift
+        ;;
+      --host|-H|--ssh-host*)
+        SSH_HOSTNAME="$2"
+        shift 2
+        ;;
+      --port|-P|--ssh-port)
+        SSH_PORT="$2"
+        shift 2
+        ;;
+      --username|--user|-u|--ssh-user*)
+        SSH_USERNAME="$2"
+        shift 2
+        ;;
+      --ssh-key|--key|--private-key|--pkey|-k)
+        SSH_KEY="$2"
+        shift 2
+        ;;
+      --sleep-interval|--sleep|-s|--interval|-i)
+        SLEEP_INTERVAL="$2"
+        shift 2
+        ;;
+      --luks-type|--type|-t)
+        LUKS_TYPE="$2"
+        shift 2
+        ;;
+      --luks-password|--password|-p)
+        LUKS_PASSWORD="$2"
+        shift 2
+        ;;
+      --remote-check|--healthcheck-remote-cmd|--remote-command|--remote-cmd|--rcmd)
+        HEALTHCHECK_REMOTE_CMD="$2"
+        shift 2
+        ;;
+      --healthcheck-host*|--hc-host*)
+        HEALTHCHECK_REMOTE_HOSTNAME="$2"
+        shift 2
+        ;;
+      --healthcheck-user*|--hc-user*)
+        HEALTHCHECK_REMOTE_USERNAME="$2"
+        shift 2
+        ;;
+      --event-file|--event|-e)
+        EVENTS_FILE="$2"
+        shift 2
+        ;;
+      --apprise-url|--apprise|-a)
+        APPRISE_URL="$2"
+        shift 2
+        ;;
+      --apprise-tag|--tag)
+        APPRISE_TAG="$2"
+        shift 2
+        ;;
+      --apprise-title|--title)
+        APPRISE_TITLE="$2"
+        shift 2
+        ;;
+      *)
+        usage >&2
+        exit 2
+        break
+        ;;
+    esac
+  done
 
   if [[ -n "$DEBUG" ]]
   then
@@ -241,14 +260,14 @@ then
     exit 2
   fi
 
-  log "LUKS rigmarole started. I'll be trying to unlock ${SSH_HOST}"
+  log "LUKS rigmarole started. I'll be trying to unlock ${SSH_HOSTNAME}"
 
   while true
   do
     # Perform Healthcheck if required
     if [[ -n "$HEALTHCHECK_PORT" ]]
     then
-      if nc -z -w 2 "$SSH_HOST" "$HEALTHCHECK_PORT"
+      if nc -z -w 2 "$SSH_HOSTNAME" "$HEALTHCHECK_PORT"
       then
         if [[ -n "$DEBUG" ]]
         then
@@ -261,7 +280,9 @@ then
 
     if [[ -n "$HEALTHCHECK_REMOTE_CMD" ]]
     then
-      if _ssh "$HEALTHCHECK_REMOTE_CMD"
+      if SSH_HOSTNAME=${HEALTHCHECK_REMOTE_HOSTNAME:-$SSH_HOSTNAME} \
+         SSH_USERNAME=${HEALTHCHECK_REMOTE_USERNAME:-$SSH_USERNAME} \
+        _ssh "$HEALTHCHECK_REMOTE_CMD"
       then
         if [[ -n "$DEBUG" ]]
         then
@@ -272,17 +293,17 @@ then
       fi
     fi
 
-    if nc -z -w 2 "$SSH_HOST" "$SSH_PORT"
+    if nc -z -w 2 "$SSH_HOSTNAME" "$SSH_PORT"
     then
-      log "Trying to unlock remotely ${SSH_HOST}"
+      log "Trying to unlock remotely ${SSH_HOSTNAME}"
       if luks_unlock
       then
-        log-notify -s "LUKS unlocked host at $SSH_HOST"
+        log-notify -s "LUKS unlocked host at $SSH_HOSTNAME"
       else
-        log-notify -f "Failed to unlock $SSH_HOST" >&2
+        log-notify -f "Failed to unlock $SSH_HOSTNAME" >&2
       fi
     else
-      log "$SSH_HOST is not reachable on port $SSH_PORT" >&2
+      log "$SSH_HOSTNAME is not reachable on port $SSH_PORT" >&2
     fi
 
     sleep "$SLEEP_INTERVAL"
