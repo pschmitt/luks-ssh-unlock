9b503e068ed611a813515ed831c0c71672580eac
Philipp Schmitt <philipp@schmitt.co>
2025-12-03 11:57:58 +0100

    Remove legacy platforms

--- buildx.sh
+++ buildx.sh
@@ -3,6 +3,6 @@
 cd "$(readlink -f "$(dirname "$0")")" || exit 9
 
 docker buildx build \
-  --platform=linux/amd64,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64/v8 \
+  --platform=linux/amd64,linux/arm64/v8 \
   --tag pschmitt/luks-ssh-unlock \
   --push .
