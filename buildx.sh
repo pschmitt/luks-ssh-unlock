d05b5c512459cb0de7c309178844296069f82da3
Philipp Schmitt <philipp@schmitt.co>
2025-12-03 12:03:13 +0100

    Rewamp docker builds

--- buildx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(readlink -f "$(dirname "$0")")" || exit 9
-
-docker buildx build \
-  --platform=linux/amd64,linux/arm64/v8 \
-  --tag pschmitt/luks-ssh-unlock \
-  --push .
